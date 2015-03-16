5e9a70d8fe1a98dce936ec66fbcf5a52b0b2271d
Tyler Brown <elementalplug@gmail.com>
2015-03-16 14:53:31 -0700

    Adds show, edit, and update to the lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,31 @@ class ListsController < ApplicationController
     render :new
   end
 
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   def show
     @list = List.find(params[:id])
     render :show
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
   end
 end
